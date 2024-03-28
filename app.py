0e9f643b50c7a00472a5b792e27194104276cb1b
charlotte <charlotte>
2024-03-28 13:52:01 +0100

    gui running

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ from framepoid import FramePoid
 
 class App(ttk.Window):
     def __init__(self):
-        super().__init__(title="Me-Mow",themename="superhero")
+        super().__init__(title="Me-Mow",themename="darkly")
 
         self.book = ttk.Notebook(self)
         self.book.pack()
         self.framenote = FrameNote(self.book)
-        self.book.add(self.framenote, text="Note")
+        self.book.add(self.framenote, text="Notes")
 
         self.framecal = FrameCal(self.book)
         self.book.add(self.framecal, text="calendrier")
 
         self.framepoid = FramePoid(self.book)
-        self.book.add(self.framepoid, text="poid")
+        self.book.add(self.framepoid, text="poids et taille")
 
 
 if __name__ == "__main__":
--- framecal.py
+++ framecal.py
@@ -6,19 +6,19 @@ class FrameCal(ttk.Frame):
         frame_left = ttk.Frame(self, borderwidth=2)
         frame_left.pack(side=ttk.LEFT, fill=ttk.Y, expand=ttk.YES)
     
-        btn_create = ttk.Button(frame_left, text='create', style='info.TButton')
+        btn_create = ttk.Button(frame_left, text='create', bootstyle='secondary')
         btn_create.pack(side=ttk.TOP,fill=ttk.X, padx=10, pady=10)
 
-        btn_save = ttk.Button(frame_left, text='save', style='info.TButton')
+        btn_save = ttk.Button(frame_left, text='save',bootstyle='secondary')
         btn_save.pack(side=ttk.TOP,fill=ttk.X, padx=10, pady=10)
 
-        btn_mod = ttk.Button(frame_left, text='modify', style='info.TButton')
+        btn_mod = ttk.Button(frame_left, text='modify', bootstyle='secondary')
         btn_mod.pack(side=ttk.TOP,fill=ttk.X, padx=10, pady=10)
 
-        btn_del = ttk.Button(frame_left, text='delete', style='info.TButton')
+        btn_del = ttk.Button(frame_left, text='delete', bootstyle='secondary')
         btn_del.pack(side=ttk.TOP,fill=ttk.X, padx=10, pady=10)
 
-        btn_quit = ttk.Button(frame_left, text='quit', style='light.TButton')
+        btn_quit = ttk.Button(frame_left, text='quit', bootstyle='secondary')
         btn_quit.pack(side=ttk.TOP,fill=ttk.X, padx=10, pady=10)
     
         def make_center_frame():

37c01c1258cef9736b57be592aa9e93e3b0612d6
charlotte <charlotte>
2024-04-13 17:08:07 +0200

    gui treeview running

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageTk
+from PIL import Image
 
-root = tk.Tk()
-root.title("Affichage d'une photo avec ttkbootstrap")
-
-
-image_path = "pitichat.jpg"
-image = Image.open(image_path)
-photo = ImageTk.PhotoImage(image)
-
-
-label = ttk.Label(root, image=photo)
-label.pack()
-
-root.mainloop()
+image = Image.open('pitichat.jpg')
+image.show()
